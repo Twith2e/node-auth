3d3b92b066a9956ed03437473934b10e097923a5
twith2e <abdulbasitadebajo1619@gmail.com>
2024-10-14 04:44:13 +0100

    added regex validation

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+let userName;
 const usersFilePath = path.join(__dirname, "db.json");
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
 // Function to read users from the file
 function readUsersFromFile() {
@@ -28,11 +31,11 @@ app.listen(port, () => {
 });
 
 app.get("/home", (req, res) => {
-  res.render("index"); // Handle GET request for /signup
+  res.render("index", { name: userName }); // Handle GET request for /signup
 });
 
 app.get("/", (req, res) => {
-  res.render("signup"); // Handle GET request for /
+  res.render("signup", { errorMessage: null }); // Handle GET request for /
 });
 
 app.post("/log", (req, res) => {
@@ -44,6 +47,7 @@ app.post("/log", (req, res) => {
       (user) => user.username === username && user.password === password
     )
   ) {
+    userName = username;
     res.redirect("/home");
   } else {
     res.render("login", { errorMessage: "Invalid username or password" });
@@ -58,15 +62,22 @@ app.post("/register", (req, res) => {
   let users = readUsersFromFile();
   const username = req.body.username;
   const password = req.body.password;
+  const confirmPassword = req.body.confirmpassword;
 
   // Check if the username already exists
-  if (users.find((user) => user.username === username)) {
-    return res.render("signup", { errorMessage: "Username already exists" });
+  if (password !== confirmPassword) {
+    res.render("signup", { errorMessage: "Passwords do not match" });
+  } else if (!passwordRegex.test(password)) {
+    res.render("signup", {
+      errorMessage:
+        "Password must contain 1 uppercase letter, 1 lowercase letter, 1 digit, 1 of these special characters(@$!%*?&) and must be at least 8 characters long",
+    });
+  } else {
+    if (users.find((user) => user.username === username)) {
+      return res.render("signup", { errorMessage: "Username already exists" });
+    }
+    users.push({ username, password });
+    writeUsersToFile(users);
+    res.redirect("/login");
   }
-
-  users.push({ username, password });
-  writeUsersToFile(users);
-  res.redirect("/login");
-
-  // res.render("login"); // Handle POST request for /login
 });
